1c2ccdac374b0df55e71786bd791da70821ab11d
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-24 23:05:21 -0700

    Create DividingIntegers.c

---
+++ DividingIntegers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(main){
+int a,b;
+printf("Enter two integers, a and b and compute a/b");
+scanf("%d %d", &a, &b);
+printf("%d / %d = %d\n", a, b, a/b);
+printf("remainder is %d\n", a%b);
+exit(0);
+}
