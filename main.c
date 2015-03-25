d076c8e95774ef40d2bfa7c046aaa013a590068c
aliciaguerra <aiguerra@ucsd.edu>
2015-03-24 22:50:00 -0700

    C
    basic C programs

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: aiguerra
+ *
+ * Created on March 24, 2015, 10:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
