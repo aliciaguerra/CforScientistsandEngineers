dbd531b7a4b57fc823a07f750826cab8b1ba0a25
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-24 22:54:16 -0700

    Create AddingIntegers.c

---
+++ AddingIntegers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(main)
+{
+int num1, num2, num3;
+printf("Enter two integers for addition:");
+scanf("%d %d", &num1,num2);
+num3=num1+num2;
+printf("Addition: %d + %d = %d\n", num1, num2, num3);
+}
