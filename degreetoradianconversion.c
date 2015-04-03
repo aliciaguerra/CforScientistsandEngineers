adb27ebb4a90bd272f98530ba38ef9a4392f91a3
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-02 20:56:23 -0700

    Create degreetoradianconversion.c

---
+++ degreetoradianconversion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.141592654
+int main(void){
+int degrees;
+double radians;
+degrees=0;
+printf("Degrees to Radians\n");
+while(degrees<=360){
+radians=degrees*PI/180.;
+printf("%6d %9.6f\n", degrees, radians);
+degrees += 20;
+}
+exit(0);
+}
