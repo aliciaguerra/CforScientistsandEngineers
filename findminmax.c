b37860b58d17e877ad417e8ede5386487a65e31b
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-03 11:05:16 -0700

    Update findminmax.c

--- findminmax.c
+++ findminmax.c
@@ -3,5 +3,24 @@ Compute y(x) = 4x^2-3x+2 for -10<=x<=10 at the increment of del_x=0.5. and find 
 */
 #include<stdio.h>
 int main(void)
+{
+  double x, y, y_max, y_min, del_x,=0.5;
+  x=-10;
+/*compute y(-10) to initialize y_min and y_max*/
+y=4.*x*x - 3.*x + 2;
+y_max=y;
+y_min=y;
+printf("\t x\t\t y(x)\n");
+do{
+  y=4.*x*x-3.*x+2;
+  if(y>y_max) y_max=y;
+  if(y<y_min) y_min=y;
+  printf("%f\t %e\n", x, y);
+  /*update x*/
+  x += del_x;
+} while(x<=10.000001);/*to execute the case x=10.*/
+/***A for-loop is simpler than do() while()***/
+printf("y_max=%g \t y_min=%g", y_max, y_min);
+}
 
 
