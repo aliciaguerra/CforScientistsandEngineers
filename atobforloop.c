1bbe589ce018c2ba24fa707ae465a1ba85cc2bee
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-03 11:38:24 -0700

    Update atobforloop.c

--- atobforloop.c
+++ atobforloop.c
@@ -7,9 +7,10 @@ int main(void)
 int i, n_end=30;
 double f, x, a=0.0, b=3.0;
 del_x=(b-a)/(double)n_end;
-for(i=0; i<n_trap+1; i++){
-x=a+(double)i*del_x;
-f=1.+x*x;
-printf("i=%3d\t x=%6.2f \t f=%12.6e\n", i, x, f);
+for(i=0; i<=n_end; i++){
+  x=a+(double)i * del_x;
+  f=1.+ x*x;
+  printf("i=%3d\t x=%6.2f \t f=%12/6e\n", i, x, f);
+}
 exit(0);
 }
